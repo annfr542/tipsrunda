c27e7aaff45451821d4ea103f3325bfda2676c60
Anna <annfr542@student.liu.se>
2020-09-02 11:28:18 +0200

    Updates

---
+++ precache-manifest.dc193b6616ab8a6e31b570628720a9e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b029759687d49c2d71a81a98240d0e4d",
+    "url": "/tipsrunda/index.html"
+  },
+  {
+    "revision": "ab2fb2693778615b6e3c",
+    "url": "/tipsrunda/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5656d478acc771b19c3b",
+    "url": "/tipsrunda/static/css/main.808dd354.chunk.css"
+  },
+  {
+    "revision": "ab2fb2693778615b6e3c",
+    "url": "/tipsrunda/static/js/2.727c4e4f.chunk.js"
+  },
+  {
+    "revision": "746288ad5703744478dcc4ae7bf871ce",
+    "url": "/tipsrunda/static/js/2.727c4e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5656d478acc771b19c3b",
+    "url": "/tipsrunda/static/js/main.097f95b2.chunk.js"
+  },
+  {
+    "revision": "902d58971ca66be80659",
+    "url": "/tipsrunda/static/js/runtime-main.70703a9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tipsrunda/static/media/logo.5d5d9eef.svg"
+  }
+]);
